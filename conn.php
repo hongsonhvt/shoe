e493d87effc04efcc75b759cdd47367e5265c1de
hongsonhvt <80675760+hongsonhvt@users.noreply.github.com>
2021-05-11 21:06:18 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,12 +1,12 @@
-<?php 
-$servername="ec2-54-87-112-29.compute-1.amazonaws.com";
-$username ="xbfdbykydcdqey";
-$password="72076682bd6d235a22ba49743a4822f1a592f4634c46a9d546c4e2d3298ee0f2";
-$dbname= "d81h7m17t2ju55";
-$conn= mysqli_connect($servername,$username,$password,$dbname);
-  if(!$conn)
-  {
-    die("Error Connection". mysqli_connect_error());
-  }
-  
- ?>
+<?php
+$host = "ec2-54-161-239-198.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d81h7m17t2ju55";
+$user = "xbfdbykydcdqey";
+$password = "72076682bd6d235a22ba49743a4822f1a592f4634c46a9d546c4e2d3298ee0f2"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+$dbconn = pg_connect($connection_string);
+if(!$dbconn){
+    echo "loi";
+}
+?>
